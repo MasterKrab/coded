97ffd752f04fb06bfbaf9ae59f8183a4aca62b84
Enzo <enzovivallo05@outlook.com>
2021-08-24 00:23:26 -0400

    Fix image meta tag

--- components/PostMeta/index.js
+++ components/PostMeta/index.js
@@ -5,15 +5,19 @@ const PostMeta = ({ title, description, image, slug }) => (
     <title>{title}</title>
     <meta name="description" content={description} />
     <meta property="og:title" content={title} />
-    <meta
-      property="og:description"
-      content={`https://www.coded.tech/${image}`}
-    />
-    {image && <meta property="og:image" content={image} />}
+    <meta property="og:description" content={description} />
+    {image && (
+      <>
+        <meta property="og:image" content={`https://www.coded.tech/${image}`} />
+        <meta
+          name="twitter:image"
+          content={`https://www.coded.tech/${image}`}
+        />
+      </>
+    )}
     <meta property="og:url" content={`https://www.coded.tech/${slug}`}></meta>
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={description} />
-    <meta name="twitter:image" content={`https://www.coded.tech/${image}`} />
     <meta name="twitter:card" content="summary_large_image" />
   </Head>
 )
