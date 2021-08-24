ce21e957e86fce1464f6ed821d55fc84a9e6251c
Enzo <enzovivallo05@outlook.com>
2021-08-24 00:31:35 -0400

    Add app head meta tags

--- components/AppHead/index.js
+++ components/AppHead/index.js
@@ -8,9 +8,13 @@ const AppHead = () => (
       href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap"
       rel="stylesheet"
     />
-    <link rel="icon" href="/assets/logo.svg" />
-    <meta name="og:image" content="/thumbnail.png" />
-    <meta name="twitter:image" content="/thumbnail.png" />
+    <link rel="icon" href="https://www.coded.tech/assets/logo.svg" />
+    <meta property="og:image" content="https://www.coded.tech/thumbnail.png}" />
+    <meta
+      name="twitter:image"
+      content="https://www.coded.tech/thumbnail.png}"
+    />
+    <meta name="twitter:card" content="summary_large_image" />
   </Head>
 )
 
