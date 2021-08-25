1bfab1f8ecf24296472fa2476e60e44d468f3607
Enzo <enzovivallo05@outlook.com>
2021-08-25 19:53:19 -0400

    Minor adjustments to postCard title styles

--- components/PostCard/styles.js
+++ components/PostCard/styles.js
@@ -33,7 +33,8 @@ export const Card = styled.article`
 `
 
 export const Title = styled.h2`
-  font-size: 1em;
+  font-size: 1.15em;
+  margin-top: 0.25rem;
   margin-bottom: 0.5rem;
 
   &::before {
