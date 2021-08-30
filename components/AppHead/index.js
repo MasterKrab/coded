9ee48d79df04743728ee9b9fa3f3000202cbb99c
Enzo <enzovivallo05@outlook.com>
2021-08-29 23:42:17 -0400

    Fix favicons files and add fonts in another file

--- components/AppHead/index.js
+++ components/AppHead/index.js
@@ -1,14 +1,10 @@
 import Head from 'next/head'
+import Fonts from './Fonts'
 import Favicons from './Favicons'
 
 const AppHead = () => (
   <Head>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap"
-      rel="stylesheet"
-    />
+    <Fonts />
     <Favicons />
     <meta property="og:type" content="website"></meta>
     <meta
