f5c63b4a0a12d668b5aeb87354387757263edc39
Enzo <enzovivallo05@outlook.com>
2021-09-01 01:59:37 -0400

    Refactor

--- components/PostComments/index.js
+++ components/PostComments/index.js
@@ -1,42 +1,24 @@
-import { useContext, useEffect, useRef } from 'react'
+import { useContext } from 'react'
 import ThemeContext from 'context/theme'
 import THEME_STATES from 'context/theme/states'
 import { Container } from './styles'
+import { Giscus } from '@giscus/react'
 
 const PostComments = () => {
   const { themeDevice } = useContext(ThemeContext)
-  const container = useRef()
-
-  useEffect(() => {
-    const scriptElement = document.createElement('script')
-    scriptElement.async = true
-    scriptElement.src = 'https://giscus.app/client.js'
-    scriptElement.setAttribute('data-repo', 'MasterKrab/coded')
-    scriptElement.setAttribute(
-      'data-repo-id',
-      'MDEwOlJlcG9zaXRvcnkzOTc3Mjc1NjM'
-    )
-    scriptElement.setAttribute('data-category', 'Comments')
-    scriptElement.setAttribute('data-category-id', 'DIC_kwDOF7TXS84B-5Hs')
-    scriptElement.setAttribute('data-mapping', 'og:title')
-    scriptElement.setAttribute('data-reactions-enabled', '1')
-    scriptElement.setAttribute('data-emit-metadata', '0')
-    scriptElement.setAttribute('crossorigin', 'anonymous')
-
-    scriptElement.setAttribute(
-      'data-theme',
-      themeDevice === THEME_STATES.DARK ? 'dark_dimmed' : 'light'
-    )
-
-    const ContainerComments = container.current.firstElementChild
-
-    ContainerComments.textContent = ''
-    ContainerComments.appendChild(scriptElement)
-  }, [container.current, themeDevice])
 
   return (
-    <Container aria-label="Comments" ref={container}>
-      <div />
+    <Container aria-label="Comments">
+      <Giscus
+        repo="MasterKrab/coded"
+        repoId="MDEwOlJlcG9zaXRvcnkzOTc3Mjc1NjM"
+        category="Comments"
+        categoryId="DIC_kwDOF7TXS84B-5Hs"
+        mapping="og:title"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        theme={themeDevice === THEME_STATES.LIGHT ? 'light' : 'dark_dimmed'}
+      />
     </Container>
   )
 }
