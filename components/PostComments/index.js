523dcec9a8bc18873ade831f08c12f7da43489de
Restyled.io <commits@restyled.io>
2021-09-01 06:02:03 +0000

    Restyled by clang-format

--- components/PostComments/index.js
+++ components/PostComments/index.js
@@ -1,26 +1,29 @@
-import { useContext } from 'react'
+import {Giscus} from '@giscus/react'
 import ThemeContext from 'context/theme'
 import THEME_STATES from 'context/theme/states'
-import { Container } from './styles'
-import { Giscus } from '@giscus/react'
+import {useContext} from 'react'
 
-const PostComments = () => {
-  const { themeDevice } = useContext(ThemeContext)
+import {Container} from './styles'
+
+const PostComments =
+    () => {
+      const {themeDevice} = useContext(ThemeContext)
 
   return (
     <Container aria-label="Comments">
       <Giscus
-        repo="MasterKrab/coded"
-        repoId="MDEwOlJlcG9zaXRvcnkzOTc3Mjc1NjM"
-        category="Comments"
-        categoryId="DIC_kwDOF7TXS84B-5Hs"
-        mapping="og:title"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        theme={themeDevice === THEME_STATES.LIGHT ? 'light' : 'dark_dimmed'}
+  repo = "MasterKrab/coded"
+  repoId = "MDEwOlJlcG9zaXRvcnkzOTc3Mjc1NjM"
+  category = "Comments"
+  categoryId = "DIC_kwDOF7TXS84B-5Hs"
+  mapping = "og:title"
+  reactionsEnabled = "1"
+  emitMetadata = "0"
+        theme={
+    themeDevice === THEME_STATES.LIGHT ? 'light' : 'dark_dimmed'}
       />
     </Container>
   )
-}
+    }
 
 export default PostComments
