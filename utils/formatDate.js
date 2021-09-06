25f4bc70e1bc695cd53a34f72a083d4dd6dee42b
Enzo <enzovivallo05@outlook.com>
2021-09-05 23:55:24 -0300

    Fix date format

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,12 +1,12 @@
 import monthsSlugs from 'utils/monthsSlugs'
 
-const formatUnit = (date) => (date < 10 ? `0${date}` : date)
-
 const formatDate = (date) => {
-  const [day, month, year] = date.split('-')
+  const [year, month, day] = date.split('-')
+
+  const normalizedMonth = month.replace(/(^|-)0+/g, '$1')
 
   const normalizedDate = Date.parse(
-    `${formatUnit(day)} ${monthsSlugs[month]} ${year}`
+    `${day} ${monthsSlugs[normalizedMonth]} ${year}`
   )
 
   return new Intl.DateTimeFormat('es-ES', {
