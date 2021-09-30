eea7a34177fd28348893d0073999c7c11f1af526
Enzo <enzovivallo05@outlook.com>
2021-09-30 17:10:49 -0300

    Add custom error 500

--- pages/404/index.js
+++ pages/404/index.js
@@ -1,18 +1,24 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import { ErrorContainer } from 'components/Error'
-import { Title, StyledLink, Image } from 'styles/pages/404.styles'
+import { Title, StyledLink, Image } from 'styles/pages/error.styles'
 import getRandomUntil from 'utils/getRandomUntil'
 
 const Custom404 = () => (
-  <ErrorContainer>
-    <Title>Ups... No hemos podido encontrar la página que buscas</Title>
-    <Link href="/" passHref>
-      <StyledLink>Ir al inicio</StyledLink>
-    </Link>
-    <Image
-      src={`/assets/illustrations/not-found-${getRandomUntil(4)}.svg`}
-      alt="No encontrado"
-    />
-  </ErrorContainer>
+  <>
+    <Head>
+      <title>Error 404</title>
+    </Head>
+    <ErrorContainer>
+      <Title>Ups... No hemos podido encontrar la página que buscas</Title>
+      <Link href="/" passHref>
+        <StyledLink>Ir al inicio</StyledLink>
+      </Link>
+      <Image
+        src={`/assets/illustrations/not-found-${getRandomUntil(4)}.svg`}
+        alt="No encontrado"
+      />
+    </ErrorContainer>
+  </>
 )
 export default Custom404
