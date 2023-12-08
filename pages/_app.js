4191730dbee16ed7543c95dc6fb7f25eeb9cc74b
Enzo <47456460+MasterKrab@users.noreply.github.com>
2023-12-08 15:09:10 -0300

    fix: remove newsletter
    Remove the newletter component from the application without deleting the component in the project files because the newsletter service is not working

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import ProgressBar from 'components/ProgressBar'
 import SkipToMain from 'components/SkipToMain'
 import Header from 'components/Header'
 import { Main } from 'styles/pages/app-styles'
-import Newsletter from 'components/Newsletter'
 import LinkToTop from 'components/LinkToTop'
 import Footer from 'components/Footer'
 import cookie from 'cookie'
@@ -27,7 +26,6 @@ const MyApp = ({ Component, pageProps, themeDevice, theme }) => (
       <Main id="main">
         <Component {...pageProps} />
       </Main>
-      <Newsletter />
       <LinkToTop />
       <Footer />
     </StylesProviders>
